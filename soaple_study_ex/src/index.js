2023b5a2337ab042763db21fb8fa62377fb47ede
MinbokLee <lik707@naver.com>
2025-09-17 16:55:25 +0900

    chapter11~12 update

--- soaple_study_ex/src/index.js
+++ soaple_study_ex/src/index.js
@@ -4,20 +4,22 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-import Library from './chapter03/Library';  
-import Clock from './chapter04/Clock';
-import CommentList from './chapter05/CommentList';
-import NotificationList from './chapter06/NotificationList';  
-import Accommdoate from './chapter07/Accommodate';
-import ConfirmButton from './chapter08/ConfirmButton';
-import ConfirmButton1 from './chapter08/ConfirmButton1';
-import LandinfPage from './chapter09/LandingPage';
-import AttendanceBook from './chapter10/AttendanceBook';
+// import Library from './chapter03/Library';  
+// import Clock from './chapter04/Clock';
+// import CommentList from './chapter05/CommentList';
+// import NotificationList from './chapter06/NotificationList';  
+// import Accommdoate from './chapter07/Accommodate';
+// import ConfirmButton from './chapter08/ConfirmButton';
+// import ConfirmButton1 from './chapter08/ConfirmButton1';
+// import LandinfPage from './chapter09/LandingPage';
+// import AttendanceBook from './chapter10/AttendanceBook';
+//import SignUp from './chapter11/SignUp';
+import Calculator from './chapter12/Calculator'; 
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <AttendanceBook />
+    <Calculator />
   </React.StrictMode>
 
 );
